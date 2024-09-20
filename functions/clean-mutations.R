afdfc572306a6df8bc32e3adf9c381d6daf253d9
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2024-09-20 11:24:51 +0100

    provisional function to get the mutations from the gene-codonamino (i.e. k13-580Y) format to the unified long string format. still need to figure out haplotypes and other special cases

---
+++ functions/clean-mutations.R
@@ -0,0 +1,21 @@
+# function to clean mutation names from the old format such as those used by WWARN into the long string
+# format that we decided upon
+
+clean_mutations <- function(string) {
+  gene <- str_split(string,"-")[[1]][1]
+  mut <- str_split(string,"-")[[1]][2]
+  amino <- gsub("[^a-zA-Z]", "", mut)
+  
+  if(amino == "WT") {
+    gene_mut <- paste0(gene,":",amino)
+  } else {
+    codon <- parse_number(mut)
+    gene_mut <- paste0(gene,":",codon,":",amino)
+  }
+  return(gene_mut)
+}
+
+## test function
+# clean_mutations("mdr1-86Y")
+# clean_mutations("k13-WT")
+# clean_mutations("k13-580Y")
