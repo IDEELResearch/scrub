e360146110ca808bfeb50dc117e7deaeb065d18e
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2024-09-20 15:55:21 +0100

    will need a new format but OJ to clean up the structure of the repo - doing this to avoid issues in my command line

--- functions/clean-mutations.R
+++ functions/clean-mutations.R
@@ -1,7 +1,35 @@
 # function to clean mutation names from the old format such as those used by WWARN into the long string
 # format that we decided upon
+# updated to handle the formats in the pf7k database and pull out the things we are interested in
 
 clean_mutations <- function(string) {
+  string <- str_replace_all(string, "_", "-")
+  gene <- str_split(string,"-")[[1]][1]
+  mut <- str_split(string,"-")[[1]][2]
+  amino <- gsub("[^a-zA-Z/|]", "", mut) # add symbols indicating mixed infections
+  
+  if(amino == "WT") {
+    gene_mut <- paste0(gene,":",amino)
+  } else if(amino == "CNV"){
+    gene_mut <- paste0(gene,":",amino)
+  } 
+  else {
+    codon <- parse_number(mut)
+    gene_mut <- paste0(gene,":",codon,":",amino)
+  }
+  return(gene_mut)
+}
+
+# FOR UNDERSTANDING
+# Upper case = homozygous mutations
+# lower case = heterozygous
+# lower case without second mutation noted is heterozygous at SNP level but same AA
+# , dictates separate and distinct haplotypes (different clones)
+# / dictates additional NS changes in the same clone
+# * in the context of mutations, these indicate that the sample could not be phased
+
+# specific function for dealing with the features of the pf7k k13 encoding
+clean_pf7k_k13 <- function(string) {
   string <- str_replace_all(string, "_", "-")
   gene <- str_split(string,"-")[[1]][1]
   mut <- str_split(string,"-")[[1]][2]
