b65fde1b2db9275fe2c1c196be243a363d062351
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2024-09-21 15:27:04 +0100

    example workflow with WWARN and geoff data using placeholder geoff data. this will need to be updated when STAVE is finalised

---
+++ analysis/example-workflow.R
@@ -0,0 +1,50 @@
+## example workflow to read in all the data files and convert them to a stave object
+devtools::load_all() # make sure all functions & packages are loaded that we need in this library
+
+# load stave - currently STAVE is empty 
+devtools::install_github("mrc-ide/STAVE")
+library(STAVE)
+
+# TODO: this will all need fixing once Bob fixes stave
+
+# read in data from databases (excluding geoff)
+wwarn_data <- readRDS("analysis/data-derived/wwarn_res_df.rds")
+wwarn_stave <- wwarn_to_stave(wwarn_data = wwarn_data)
+
+# do the same for the other databases
+
+# make an empty stave object
+stave <- STAVE::STAVE_object()
+# append WWARN data
+stave <- STAVE::append_data(stave, wwarn_stave)
+
+# now convert all of the geoff objects into a list
+geoff_path <- "analysis/data-geoff/" # filepath to all of the geoff tsvs which are in separate folders
+folders <- list.dirs(path = geoff_path)
+
+geoff <- list(studies_dataframe = data.frame(),
+              surveys_dataframe = data.frame(),
+              counts_dataframe = data.frame())
+
+for(i in 2:length(folders)) { # the first path is the root which we want to omit
+  files <- list.files(path = folders[i],
+                      all.files = TRUE, full.names = TRUE) %>%
+    str_subset(".tsv")
+  # relies on a standardised naming convention of the .tsv files
+  study_path <- str_subset(files, "study")
+  site_path <- str_subset(files, "site")
+  prev_path <- str_subset(files, "prevalence")
+  
+  geoff_obj <- read_geoff(study_path = study_path,
+                          site_path = site_path,
+                          prev_path = prev_path)
+  
+  # this is now a list of data frames ready for STAVE
+  geoff <- list(studies_dataframe = rbind(geoff$studies_dataframe, geoff_obj$studies_dataframe),
+                surveys_dataframe = rbind(geoff$surveys_dataframe, geoff_obj$surveys_dataframe),
+                counts_dataframe = rbind(geoff$counts_dataframe, geoff_obj$counts_dataframe))
+  
+}
+
+stave <- STAVE::append_data(stave, geoff)
+
