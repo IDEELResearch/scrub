a7208006c477102903d29ad18a7cf79b0031365d
gtollefson <george_tollefson@brown.edu>
2024-10-21 17:28:04 -0400

    wrote 01_read and 02_clean scripts for merging all geoff studies and cleaning for various formatting inconsistencies and shaping for rbind with wwarn

--- analysis/01_read_geoffs.R
+++ analysis/01_read_geoffs.R
@@ -1,24 +1,61 @@
-# now convert all of the geoff objects into a list
-geoff_path <- "analysis/data-geoff/" # filepath to all of the geoff tsvs which are in separate folders
-folders <- list.dirs(path = geoff_path)
+# Load required libraries
+library(dplyr)
+library(lubridate)
+library(here)
 
-geoff <- data.frame()
+################################################################################
+#
+# Script: 01_read_geoffs.R
+# Purpose: Combine all geoff studies into one dataframe
+#
+################################################################################
 
-for(i in 2:length(folders)) { # the first path is the root which we want to omit
-  files <- list.files(path = folders[i],
-                      all.files = TRUE, full.names = TRUE) %>%
-    str_subset(".tsv")
-  # relies on a standardised naming convention of the .tsv files
-  # TODO: update this to reflect the real unique string
-  study_path <- str_subset(files, "study")
-  site_path <- str_subset(files, "site")
-  prev_path <- str_subset(files, "prevalence")
+# Define root directory and initialize empty dataframe for combined all studies wide table
+root_dir <- here("analysis", "data-geoff")
+master_table <- data.frame()
+
+# Iterate over each project directory within root_dir
+study_folders <- list.dirs(root_dir, recursive = FALSE)
+for (project_dir in study_folders) {
   
-  geoff_obj <- read_geoff(study_path = study_path,
-                          site_path = site_path,
-                          prev_path = prev_path)
+  # Extract the project identifier
+  study_uid <- basename(project_dir)
+  print(paste("Processing project:", study_uid))
   
-  # this is now a list of data frames ready for STAVE
-  geoff <- rbind(geoff, geoff_obj)
+  # Identify file paths
+  study_overview_path <- Sys.glob(file.path(project_dir, "*_study_data_validated.tsv.gz"))
+  site_overview_path <- Sys.glob(file.path(project_dir, "*_site_data_validated.tsv.gz"))
+  prev_table_path <- Sys.glob(file.path(project_dir, "*_prevalence_data_LONG_validated.tsv.gz"))
   
+  # Skip if any files are missing
+  if (length(study_overview_path) == 0 || length(site_overview_path) == 0 || length(prev_table_path) == 0) {
+    next
+  }
+  
+  # Read in the TSV files
+  study_overview <- read.table(gzfile(study_overview_path), sep = "\t", header = TRUE, fill = TRUE)
+  site_overview <- read.table(gzfile(site_overview_path), sep = "\t", header = TRUE, fill = TRUE)
+  prev_table <- read.table(gzfile(prev_table_path), sep = "\t", header = TRUE, fill = TRUE)
+  
+  # Transpose and merge data
+  study_overview_t <- as.data.frame(t(study_overview), stringsAsFactors = FALSE)
+  colnames(study_overview_t) <- make.names(study_overview$FIELDS, unique = TRUE)
+  study_overview_t <- study_overview_t[-1, , drop = FALSE]
+  
+  wide_data <- prev_table %>%
+    left_join(site_overview, by = "site_uid") %>%
+    mutate(study_key = study_uid) %>%
+    cbind(study_overview_t) %>%
+    as.data.frame(check.names = TRUE)
+  
+  # Convert numeric columns to character
+  wide_data <- wide_data %>%
+    mutate(across(where(is.numeric), as.character))
+  
+  # Append data to master_table
+  master_table <- bind_rows(master_table, wide_data)
 }
+
+# Save the combined master table as an RDS file in the data-derived directory
+saveRDS(master_table, here("analysis", "data-derived", "01_read_geoffs_output_table.rds"))
+print("Master table saved. Ready for cleaning with 02_clean_geoffs.R.")
