8cfab442786c6609fe7cb36ee60439657c6ccd28
OJWatson <o.watson15@imperial.ac.uk>
2025-04-23 13:37:01 +0100

    lots of fixes and catches now but all working (just one block of data entry issues remain)

--- analysis/02_clean_geoff.R
+++ analysis/02_clean_geoff.R
@@ -13,6 +13,8 @@ devtools::load_all()
 # 
 ################################################################################
 
+# 1. Step 1 - Read In Data and do broad cleaning # -----------------------------
+
 # read in data
 # Load the combined geoff data table created in the first script
 master_table <- readRDS(here("analysis", "data-derived", 
@@ -83,12 +85,16 @@ master_table_clean <- master_table |>
   dplyr::mutate(iso3c = replace(iso3c, iso3c == "ERT", "ERI")) |>
   dplyr::mutate(country = replace(country, country == "Gqn", "Gnq")) |>
   dplyr::mutate(iso3c = replace(iso3c, iso3c == "GQN", "GNQ")) %>% 
+  # OJ checked these and worked out their site study type
   dplyr::mutate(site_study_type = replace(site_study_type, is.na(site_study_type) & pmid == "32459360", "COMMUNITY")) %>% 
   dplyr::mutate(site_study_type = replace(site_study_type, is.na(site_study_type) & pmid == "37670357", "COMMUNITY"))
 
 # additional cleaning to correct for malformed iso3cs
-master_table_clean <- master_table_clean %>% 
-  mutate(country = if_else(country %in% c("Ago", "Ben", "Cmr", "Caf", "Cog", "Cod", "Eth", "Gha", "Stp", "Sen", "Sdn", "Uga", "Gnq", "Gab", "Mli", "Nga", "Sle", "Som", "Zmb", "Zaf"),
+master_table_clean <- master_table_clean %>%
+  mutate(country = if_else(country %in%  c("Ago", "Ben", "Cmr", "Caf", "Cog", 
+                                           "Cod", "Eth", "Gha", "Stp", "Sen", 
+                                           "Sdn", "Uga", "Gnq", "Gab", "Mli", 
+                                           "Nga", "Sle", "Som", "Zmb", "Zaf"),
                            suppressWarnings(countrycode::countrycode(toupper(country), "iso3c", "country.name.en")),
                            country))
 
@@ -126,9 +132,14 @@ check_values_in_column(master_table_clean, "publication_status", allowed_publica
 check_values_in_column(master_table_clean, "publication_year", allowed_pub_year)
 check_values_in_column(master_table_clean, "site_study_type", allowed_site_types)
 
+# 2. Step 2 - Clean mutation names and data entry issues in these # ------------
+
 # Clean mutation names
 
 # Expand all gene mutation ranges for reference range syntax
+# TODO: These are inconcistently entered. Some use mutant_num to refer to REF ranges
+# Some go the other way. Plus many data entry issues here on review. 
+# Leaving for now and will pass back to data entry
 indices_to_transform <- which(grepl("^k13:[0-9]+-[0-9]+:\\*$", tolower(master_table_clean$gene_mutation)))
 master_table_clean$gene_mutation[indices_to_transform] <- sapply(
   master_table_clean$gene_mutation[indices_to_transform],
@@ -136,7 +147,7 @@ master_table_clean$gene_mutation[indices_to_transform] <- sapply(
   mutation_key = mutation_key
 )
 
-# And the same for the individual asterisk
+# For an individual asterisk with no mutants, we simply flip to ref and set all to total
 indices_to_transform <- which(grepl("^k13:[0-9]+:\\*$", tolower(master_table_clean$gene_mutation)) &
                                 master_table_clean$mutant_num == 0)
 master_table_clean$gene_mutation[indices_to_transform] <- as.character(sapply(
@@ -144,17 +155,33 @@ master_table_clean$gene_mutation[indices_to_transform] <- as.character(sapply(
   convert_k13_asterisk, 
   mutation_key = mutation_key
 ))
+master_table_clean$mutant_num[indices_to_transform] <- master_table_clean$total_num[indices_to_transform]
 
-# And this last one had a STOP codon based on the paper
-# No way to handle so remove
+# OJ: This last one had a STOP codon based on the paper
+# https://journals.plos.org/plosone/article/figure?id=10.1371/journal.pone.0235401.t006
+# No way to handle so remove this - there are other data points in this location so 
+# the denominator will be correctly captured still
 indices_to_transform <- which(grepl("^k13:[0-9]+:\\*$", tolower(master_table_clean$gene_mutation)))
 master_table_clean <- master_table_clean[-indices_to_transform,]
 
-# They did enter FC which is what the paper says. All samples were 0 for this, so likely
-# short hand for saying 0 F, and 0C.  So correcting to say these were all wild type S 
+# OJ: They did enter FC which is what the paper says. This is then likely an insertion rather than a SNP
+# https://malariajournal.biomedcentral.com/articles/10.1186/s12936-024-04945-8/tables/3
+# However, all entires had 0 observations of this, so we cannot simply remove as that removes
+# the denominator for this locus. So set to wildtype and mutant_num to total_num
 master_table_clean$mutant_num[master_table_clean$gene_mutation == "MDR1:1034:FC"] <- 
-  master_table_clean$total_num[master_table_clean$gene_mutation == "MDR1:1034:FC"] 
+  master_table_clean$total_num[master_table_clean$gene_mutation == "MDR1:1034:FC"]
 master_table_clean$gene_mutation[master_table_clean$gene_mutation == "MDR1:1034:FC"] <- "MDR1:1034:S"
+
+# OJ: They entered insertions, which are not SNPs so we will remove these 
+# This will remove the locus entirely but for now this is okay as it is outside propeller (may want to revisit)
+# https://malariajournal.biomedcentral.com/articles/10.1186/1475-2875-13-472/tables/1
+master_table_clean <- master_table_clean %>% filter(!(gene_mutation %in% c("K13:142:NN", "K13:142:NNN")))
+
+# Correct gene mutation format
+convmutation <- format_variants_for_stave(unique(master_table_clean$gene_mutation))
+master_table_clean$gene_mutation <- convmutation[match(master_table_clean$gene_mutation, unique(master_table_clean$gene_mutation))]
+
+# 3. Step 3 - Clean dates, lat, lon # ------------
 
 # Adjust and add dates and survey IDs
 convstart <- adjust_invalid_date(unique(master_table_clean$date_start), is_start = TRUE)
@@ -172,10 +199,6 @@ master_table_clean <- master_table_clean %>% rowwise() %>%
 master_table_clean$lat_n <- clean_excel_formulas(master_table_clean$lat_n)
 master_table_clean$lon_e <- clean_excel_formulas(master_table_clean$lon_e)
 
-# Correct gene mutation format
-convmutation <- format_variants_for_stave(unique(master_table_clean$gene_mutation))
-master_table_clean$gene_mutation <- convmutation[match(master_table_clean$gene_mutation, unique(master_table_clean$gene_mutation))]
-
 # change variable classes
 master_table_clean <- master_table_clean |>
   dplyr::mutate(collection_day = as.Date(collection_day),
@@ -190,10 +213,28 @@ master_table_clean <- master_table_clean |>
                 mut = str_extract(gene_mutation, "(?<=:).*")) |>
   dplyr::filter(total_num > 0) # zeroes are because these codons weren't genotyped
 
+# 4. Step 4 - Pre formatting other data entry issues # ------------
+
 # One manual fix to data entry issue in 
 # s0128_voumbo-matoumona_2018_v01_prevalence_data_LONG_validated.tsv.gz
 master_table_clean <- master_table_clean %>% 
   mutate(mutant_num = replace(mutant_num, mutant_num == 31 & total_num == 24 & site_uid == "koulamoutou", 21))
+
+# remove site="none" records from study_ID==S0006WrairKenreadKen for which lat and lon was not available
+# the meta file for this has a none location so it passed through
+# automatically from MIP pipeline reading metadata for samples without site info which have no lat/lon. 
+# To be fixed manually in entry eventually.
+master_table_clean <- master_table_clean[!(master_table_clean$study_uid == "s0006_wrair_kenread_ken" & 
+                                                     is.na(master_table_clean$lat_n) & 
+                                                     is.na(master_table_clean$lon_e)), ]
+
+# filter out any entries that are day0 calculated - these reflect extra data entries for total genotype counts
+# which are already captured within haplotype counts
+master_table_clean <- master_table_clean %>% 
+  dplyr::filter(grepl("day0calculated", substudy) == 0)
+
+
+# 5. Step 5 - Formatting # ------------
 
 if(sum(master_table_clean$total_num < master_table_clean$mutant_num) > 0) {
   errorCondition("Entries with more mutant samples than total samples")
@@ -208,10 +249,10 @@ column_names <- get_column_names_for_clean()
 # first add in extra information needed within column_names
 master_table_formatted <- master_table_clean %>% 
   group_by(study_uid) %>% 
-  dplyr::mutate(study_uid = janitor::make_clean_names(study_uid[1], "big_camel")) %>% 
+  dplyr::mutate(study_uid = paste0("geoff_", janitor::make_clean_names(study_uid[1], "big_camel"))) %>% 
   ungroup() %>% 
   dplyr::group_by(study_uid, site_name, collection_start) %>% 
-  dplyr::mutate(survey_ID = paste0("geoff_", study_uid[1], "_", 
+  dplyr::mutate(survey_ID = paste0(study_uid[1], "_", 
                                    janitor::make_clean_names(site_name[1], "big_camel"), "_", 
                                    lubridate::year(collection_start[1])),
                 continent = countrycode::countrycode(sourcevar = iso3c[1], "iso3c", "continent")) %>% 
@@ -250,14 +291,8 @@ master_table_formatted <- master_table_clean %>%
                 database = "database"
   )
 
-# filter out any entries that are day0 calculated - these reflect extra data entries for total genotype counts
-# which are already captured within haplotype counts
-master_table_formatted <- master_table_formatted %>% 
-  dplyr::filter(grepl("day0calculated", substudy) == 0)
 
-################################################################################
-# Impute k13 reference survey records not explicitly entered in data entry
-################################################################################
+# 6. Step 6 - Impute k13 reference survey records not explicitly entered in data entry # ------------
 
 # Extract K13 mutations from the mutation dictionary
 k13_mutations <- read_csv(here("analysis", "data-raw", "mutation_dictionary.csv")) %>%
@@ -297,7 +332,7 @@ if (nrow(duplicates) > 0) {
 # Append imputed records to master_table_formatted
 master_table_formatted <- bind_rows(master_table_formatted, imputed_data)
 
-# Check to confirm all k13_alleles (24 validated and candidate) are present for each survey (year/location) at least (should return 0)
+# Check to confirm all k13_alleles (24 validated and candidate) are present for each survey (year/location) at least (should return 0 rows)
 master_table_formatted %>% filter(gene == "k13") %>% group_by(survey_ID) %>% 
   summarise(n = str_extract_all(variant_string, "(?<=k13:)([\\d_]+)") %>%
                 unlist() %>%
@@ -309,16 +344,9 @@ master_table_formatted %>% filter(gene == "k13") %>% group_by(survey_ID) %>%
             max = as.integer(unique(k13_max)), 
             pos = sum(as.integer(unique(substr(k13_mutations$mut,1,3))) < max & 
                         as.integer(unique(substr(k13_mutations$mut,1,3))) > min)) %>% 
-  filter(n < post)
+  filter(n < pos)
 
-
-# remove site="none" records from study_ID==S0006WrairKenreadKen for which lat and lon was not available
-# the meta file for this has a none location so it passed through
-# automatically from MIP pipeline reading metadata for samples without site info which have no lat/lon. 
-# To be fixed manually in entry eventually.
-master_table_formatted <- master_table_formatted[!(master_table_formatted$study_ID == "S0006WrairKenreadKen" & 
-                                                     is.na(master_table_formatted$lat) & 
-                                                     is.na(master_table_formatted$lon)), ]
+# 7. Step 7 - Last checks # ------------
 
 # flag any study which has NA for lat or long after format conversion
 lat_missing_or_improper <- unique(master_table_formatted$study_ID[is.na(master_table_formatted$lat)])
@@ -331,15 +359,77 @@ cat("Studies with missing or improper longitudes:\n", paste(lon_missing_or_impro
 cat("Studies with missing or improper substudy values (critical for inclusion/exclusion in downstream analysis):\n", 
     paste(substudy_missing_or_improper, collapse = ", "), "\n\n")
 
-# Grab just the columns we need for pairing with WWARN etc
-master_table_simplified <- master_table_formatted %>% 
-  select(all_of(column_names))
-
 # flag all study uids which report less than 40% prevalence of validated k13 reference allele indicating entry error (using 469C arbitratily to detect erroneous user behavior) 
 low_prev_studies <- master_table_formatted %>%
   filter(variant_string == "k13:469:C", prev < 0.40) %>%
   distinct(study_ID, data_entry_author)  # Get unique pairs
 print(low_prev_studies)
+
+# 8. Step 8 - Any further specific data entry issues, likely identified when trying to read to STAVE ---------
+
+# Error - Now fixed: number of amino acid loci (5) must equal the number of codon positions (3)
+# This has now been fixed on 02_clean_geoff.R
+# e.g. crt:72_73_74_75_76:CVMNK/CVIET/SVMNK which should be crt:72_73_74_75_76:C/S_V_M/I_N/E_K/T
+
+# last fix for some incorrectly formatted loci
+issue <- c(
+  "mdr1:86_184_1246:YFD/YYD",
+  "mdr1:86_184_1246:YYD/NFD",
+  "mdr1:86_184_1246:NFD/YYD",
+  "mdr1:86_184_1246:YYY/NYD",
+  "mdr1:86_184_1246:NFY/NYD",
+  "mdr1:86_184_1246:NYD/NFD",
+  "mdr1:86_184_1246:NFD/NYD",
+  "mdr1:86_184_1246:YFD/YYD",
+  "mdr1:86_184_1246:YYD/NFD",
+  "mdr1:86_184_1246:NFD/YYD",
+  "mdr1:86_184_1246:YYY/NYD",
+  "mdr1:86_184_1246:NFY/NYD",
+  "mdr1:86_184_1246:NYD/NFD",
+  "mdr1:86_184_1246:NFD/NYD",
+  "crt:72_73_74_75_76:CVMNK/CVIET",
+  "crt:72_73_74_75_76:CVMNK/SVMNK",
+  "crt:72_73_74_75_76:CVMNK/CVIET/SVMNK"
+)
+
+# Create a helper function for readability
+fix_mutation_string <- function(issue_str) {
+  parts <- str_split(issue_str, ":")[[1]]
+  loci <- parts[1:2] %>% paste(collapse = ":")
+  
+  alleles <- parts[3] %>%
+    str_split("/") %>%
+    unlist() %>%
+    str_split("") %>%
+    simplify2array()
+  
+  allele_summary <- apply(alleles, 1, function(pos) paste(unique(pos), collapse = "/")) %>%
+    paste(collapse = "_")
+  
+  paste(loci, allele_summary, sep = ":")
+}
+
+# Use map_chr for clarity
+fixed <- map_chr(issue, fix_mutation_string)
+
+# Simple indexing to update master_table_formatted
+indices <- match(master_table_formatted$variant_string, issue)
+master_table_formatted$variant_string[!is.na(indices)] <- fixed[indices[!is.na(indices)]]
+
+# Then two other issues
+# "k13:578_579_580:BND" "crt:76:TK" 
+#https://malariajournal.biomedcentral.com/articles/10.1186/s12936-021-03713-2#Sec7
+master_table_formatted$variant_num[master_table_formatted$variant_string == "k13:578_579_580:BND"] <- 
+  master_table_formatted$total_num[master_table_formatted$variant_string == "k13:578_579_580:BND"]
+master_table_formatted$variant_string[master_table_formatted$variant_string == "k13:578_579_580:BND"] <- "k13:578_579_580:A_M_C"
+#https://malariajournal.biomedcentral.com/articles/10.1186/s12936-017-1777-0/tables/2
+master_table_formatted$variant_string[master_table_formatted$variant_string == "crt:76:TK"] <- "crt:76:T/K"
+
+# 9. Step 9 - Save Formatted Data ---------
+
+# Grab just the columns we need for pairing with WWARN etc
+master_table_simplified <- master_table_formatted %>% 
+  select(all_of(column_names))
 
 # Save the final merged_df as an RDS file
 saveRDS(master_table_simplified, here("analysis", "data-derived", "geoff_clean.rds"))
