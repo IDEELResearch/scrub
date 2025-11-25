71daf28c6d92d2534b3705987841a4c082354c4b
Gina Cuomo-Dannenburg <gmcuomo@gmail.com>
2025-11-25 13:32:05 +0100

    crt now checked -- fully clean and imputed. currently generating new stave object

--- analysis/02_clean_wwarn.R
+++ analysis/02_clean_wwarn.R
@@ -233,7 +233,7 @@ mdr1 <- wwarn_pd_clean %>% filter(gene == "mdr1") %>% distinct()
 crt <- wwarn_pd_clean %>% filter(gene == "crt") %>% distinct()
 nrow(mdr1) + nrow(crt) == nrow((wwarn_pd_clean) %>% distinct())
 
-# make a dataset with the manual fixes combined
+# make a dataset with the number of variants reported per survey
 mdr1 <- mdr1 %>% 
   mutate(variant_string = standardise_mdr1_86(variant_string)) %>% 
   filter(variant_string != "mdr1:86:F") %>%
@@ -298,7 +298,147 @@ mdr1_2_YM <- mdr1_2_groups %>% filter(pair_simple == "Y+N/Y") %>% filter(xs <= t
 mdr1 <- rbind(mdr1_1,
               mdr1_2, 
               mdr1_3) %>%
-  select(names(master_table_simplified)) %>%
+  select(names(wwarn_k13_clean)) %>%
+  mutate(prev = variant_num / total_num)
+
+# make a dataset with the number of variants per survey
+unique(crt$variant_string) # no variants we want to filter out
+
+crt <- crt %>%
+  dplyr::group_by(survey_ID, collection_day) %>%
+  mutate(n = length(unique(total_num))) %>%
+  filter(n == 1) %>% # exclude surveys with different Ns
+  ungroup() %>%
+  group_by(survey_ID, collection_day, total_num, variant_string) %>%
+  mutate(variant_num = sum(variant_num),
+         total_num = unique(total_num)) %>%
+  ungroup() %>%
+  distinct(survey_ID, collection_day, total_num, variant_string,.keep_all = TRUE) %>% 
+  group_by(survey_ID, collection_day, total_num) %>%
+  mutate(num_variants = n())
+
+table(crt$num_variants)
+
+# split by the number of variants in the survey
+crt_1 <- crt %>% filter(num_variants == 1) 
+crt_2 <- crt %>% filter(num_variants == 2)
+crt_3 <- crt %>% filter(num_variants == 3)
+
+# look at the 3 surveys with only one mutant
+# manually fix this ID: "wwarn_16216_Duah_Hohoe_2004" -- other paper is correct
+crt_1 <- crt_1 %>%
+  split(.$study_ID)
+
+# look back at PMID and manually fix
+crt_1$wwarn_16216_Duah <- crt_1$wwarn_16216_Duah %>%
+  bind_rows(crt_1$wwarn_16216_Duah, crt_1$wwarn_16216_Duah)
+
+# assume that K76T means mixed -- all values given in percentages and then converted
+# K76 20 = 26/130
+# T76 45 = 58 / 130
+# K76T 35 = 46 / 130
+crt_1$wwarn_16216_Duah$variant_string <- c("crt:76:K",
+                                           "crt:76:T",
+                                           "crt:76:K/T")
+
+crt_1$wwarn_16216_Duah$variant_num <- c(26, 58, 46)
+crt_1$wwarn_16216_Duah$prev <- crt_1$wwarn_16216_Duah$variant_num / crt_1$wwarn_16216_Duah$total_num
+
+(crt_1$wwarn_16216_Duah %>% 
+    mutate(xs = sum(variant_num)) %>% 
+    pull(xs)) == (unique(crt_1$wwarn_16216_Duah$total_num)) # checked this is correct now
+
+crt_1 <- do.call(rbind, crt_1)
+
+# now clean those with 2 mutations
+# 3 possible iterations of 2 mutations
+# add a column that indicates which of the three classes it falls into so that we can treat them separately
+crt_2_groups <- crt_2 %>%
+  group_by(survey_ID, collection_day) %>%
+  mutate(
+    pair = unique(variant_string) |> str_c(collapse = "+"),
+    pair_simple = case_when(
+      pair %in% c("crt:76:K+crt:76:T", "crt:76:T+crt:76:K")   ~ "K+T",
+      pair %in% c("crt:76:K+crt:76:K/T", "crt:76:K/T+crt:76:K") ~ "K+K/T",
+      pair %in% c("crt:76:T+crt:76:K/T", "crt:76:K/T+crt:76:T") ~ "T+K/T",
+      TRUE                                                          ~ pair
+    )
+  ) %>%
+  ungroup() 
+
+crt_2_groups <-  crt_2_groups %>%
+  group_by(survey_ID, collection_day, total_num) %>%
+  mutate(xs = sum(variant_num)) %>%
+  mutate(correct = if_else(xs == total_num, "yes", "no"))
+
+table(crt_2_groups$correct) 
+# 10 not correct -- look at these and fix
+
+# 3 studies to fix
+crt_2_fix <- crt_2_groups %>% 
+  filter(correct == "no") %>%
+  split(.$study_ID)
+
+# Olefongo is correct -- the difference are other mutations that we are not interested in.
+# means sum(x) != n but these are not missing mixed mutants based on the paper
+
+# simple typo -- from looking at the whole study and checking totals 76T should have one more variant
+crt_2_fix$wwarn_62503_Afoakwah$variant_num[1] <- 37
+sum(crt_2_fix$wwarn_62503_Afoakwah$variant_num) == crt_2_fix$wwarn_62503_Afoakwah$total_num
+
+# both have 100 prev -- look at paper
+# 13% 76T; 87% K76; N = 180
+crt_2_fix$wwarn_79899_Ngassa$variant_num <- c(23, 180 - 23)
+crt_2_fix$wwarn_79899_Ngassa$prev <- crt_2_fix$wwarn_79899_Ngassa$variant_num /
+  crt_2_fix$wwarn_79899_Ngassa$total_num
+
+crt_2_fix <- do.call(rbind, crt_2_fix) 
+
+crt_2 <- rbind(crt_2_groups %>%
+  filter(correct == "yes"), crt_2_fix)
+
+# check that we haven't lost any surveys
+length(unique(crt_2_groups$survey_ID)) == length(unique(crt_2$survey_ID))
+
+crt_3 <- crt %>% filter(num_variants == 3)
+crt_3_groups <- crt_3 %>%
+  group_by(survey_ID, collection_day, total_num) %>%
+  mutate(xs = sum(variant_num)) %>%
+  mutate(correct = if_else(xs == total_num, "yes", "no")) %>%
+  arrange(correct, survey_ID) %>%
+  split(.$correct)
+
+# fix this study: xs > n
+mensah <- crt_3_groups$no %>%
+  filter(study_ID == "wwarn_96528_Mensah")
+  
+mensah <- mensah[1:2,]
+mensah$variant_string <- c("crt:76:K", "crt:76:T")
+mensah$variant_num <- c(59, 15)
+mensah$total_num <- c(74, 74)
+mensah$prev <- mensah$variant_num / mensah$total_num
+
+bonizzoni <- crt_3_groups$no %>%
+  filter(study_ID == "wwarn_43882_Bonizzoni")
+bonizzoni$variant_num[1] <- 22
+bonizzoni$variant_num[6] <- 24
+
+bonizzoni <- bonizzoni %>%
+  mutate(prev = variant_num / total_num) %>%
+  group_by(survey_ID) %>%
+  mutate(xs = sum(variant_num)) 
+
+# check we haven't lost any surveys
+length(unique(crt_3$survey_ID)) == (length(unique(crt_3_groups$yes$survey_ID)) + 
+                                      length(unique(crt_3_groups$no$survey_ID)))
+
+crt_3 <- do.call(rbind, crt_3_groups)
+
+# just to ensure that the names are correct but no imputation waws required
+crt <- rbind(crt_1,
+              crt_2, 
+              crt_3) %>%
+  select(names(wwarn_k13_clean)) %>%
   mutate(prev = variant_num / total_num)
 
 wwarn_pd_clean <- rbind(mdr1, crt) %>% select(names(wwarn_k13_clean))
